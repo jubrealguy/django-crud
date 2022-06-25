f4192bcef35bf523934c3bd9af5028bb5c69c4fb
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-06-25 16:23:49 +0100

    perfected

--- I4G006401OSB/blog/views.py
+++ I4G006401OSB/blog/views.py
@@ -1,31 +1,35 @@
 from django.shortcuts import render
-from django.views import generic
-from .models import Post
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from blog.models import Post
 from django.urls import reverse_lazy
 
 # Create your views here.
 
-class PostListView(generic.ListView):
+class PostListView(ListView):
     model = Post
     template_name: "blog/post_list.html"
 
-class PostCreateView(generic.CreateView):
+class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
     template_name: "blog/post_form.html"
 
-class PostDetailView(generic.DetailView):
+class PostDetailView(DetailView):
     model = Post
     template_name: "blog/post_detail.html"
 
-class PostUpdateView(generic.UpdateView):
+class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
     template_name: "blog/post_form.html"
 
-class PostDeleteView(generic.UpdateView):
+class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
     success_url  = reverse_lazy("blog:all")
